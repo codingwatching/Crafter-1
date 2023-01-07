8d6dfb6febcb80e933648e6e2fd22e4d7fdc052e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-07 18:21:42 -0500

    Fix brutal readability problem

--- mods/player_mechanics/version_info.lua
+++ mods/player_mechanics/version_info.lua
@@ -4,22 +4,40 @@ register_on_joinplayer(function(player)
     --add in version info
     player:hud_add({
         hud_elem_type = "text",
-        position = {x=1, y=0},
+        position = {
+            x = 1,
+            y = 0
+        },
         name = "versionbg",
         text = "Alpha 0.07b",
         number = 0x000000,
-        offset = {x = -98, y = 20},
-        size = { x=2, y=2 },
+        offset = {
+            x = -98,
+            y = 20
+        },
+        size = {
+            x = 2,
+            y = 2
+        },
         z_index = 0,
     })
     player:hud_add({
         hud_elem_type = "text",
-        position = {x=1, y=0},
+        position = {
+            x=1,
+            y=0
+        },
         name = "versionfg",
         text = "Alpha 0.07b",
         number = 0xFFFFFF,
-        offset = {x = -100, y = 18},
-        size = { x=2, y=2 },
+        offset = {
+            x = -100,
+            y = 18
+        },
+        size = {
+            x = 2,
+            y = 2
+        },
         z_index = 0,
     })
 end)
