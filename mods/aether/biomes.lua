165652115a340189b00c1648505923ae3c0db38b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-08 01:33:16 -0500

    Overhaul stage 1

--- mods/aether/biomes.lua
+++ mods/aether/biomes.lua
@@ -1,7 +1,8 @@
 local VoxelArea = VoxelArea
-local get_name_from_content_id = minetest.get_name_from_content_id
+local get_content_id = minetest.get_content_id
 local get_perlin_map = minetest.get_perlin_map
 local get_mapgen_object = minetest.get_mapgen_object
+local vec_new = vector.new
 
 
 minetest.register_biome({
@@ -25,11 +26,8 @@ minetest.register_biome({
     humidity_point = -100,
 })
 
---this is from https://github.com/paramat/lvm_example/blob/master/init.lua
---hi paramat :D
-
--- Set the 3D noise parameters for the terrain.
-local np_terrain = {
+-- Set the 3D noise parameters for the terrain
+local noise_parameters = {
     offset = 0,
     scale = 1,
     spread = {x = 200, y = 100, z = 200},
@@ -53,49 +51,34 @@ local node2 = ""
 local vi
 local sidelen
 local permapdims3d  = {}
-local nobj_terrain = {}
 local vm = {}
 local emin = {}
 local emax = {}
-local area = VoxelArea:new({MinEdge = 0, MaxEdge = 0})
-local ni = 1
+local area = VoxelArea:new({MinEdge = vec_new(0,0,0), MaxEdge = vec_new(0,0,0)})
+local node_index = 1
 local density_noise  = {}
 
-local nvals_terrain = {}
+local perlin_data = {}
 local data = {}
 
-local c_dirt = minetest.get_content_id("aether:dirt")
-local c_stone = minetest.get_content_id("aether:stone")
-local c_air = minetest.get_content_id("air")
-local c_grass = minetest.get_content_id("aether:grass")
--- On generated function.
+local c_dirt = get_content_id("aether:dirt")
+local c_stone = get_content_id("aether:stone")
+local c_air = get_content_id("air")
+local c_grass = get_content_id("aether:grass")
 
--- 'minp' and 'maxp' are the minimum and maximum positions of the mapchunk that
--- define the 3D volume.
+local constant_area = {x = 80, y = 80, z = 80}
+
 minetest.register_on_generated(function(minp, maxp)
+
     --aether starts at 21000
     if minp.y < 21000 then
         return
     end
 
-    -- Start time of mapchunk generation.
-    --local t0 = minetest.get_us_time()/1000000
-
-    sidelen = maxp.x - minp.x + 1
-
-    permapdims3d = {x = sidelen, y = sidelen, z = sidelen}
-
-    nobj_terrain = get_perlin_map(np_terrain, permapdims3d)
-
-    nobj_terrain:get_3d_map_flat(minp, nvals_terrain)
-
-    ni = 1
-
+    perlin_data = get_perlin_map(noise_parameters, constant_area):get_3d_map_flat(minp, perlin_data)
+    node_index = 1
     vm, emin, emax = get_mapgen_object("voxelmanip")
-
-    -- area = VoxelArea:new({MinEdge = emin, MaxEdge = emax})
-    area.MinEdge = emin
-    area.MaxEdge = emax
+    area = VoxelArea:new({MinEdge = emin, MaxEdge = emax})
 
     vm:get_data(data)
 
@@ -105,7 +88,7 @@ minetest.register_on_generated(function(minp, maxp)
         vi = area:index(minp.x, y, z)
         for x = minp.x, maxp.x do
 
-            density_noise = nvals_terrain[ni]
+            density_noise = perlin_data[ni]
 
             if density_noise > 0.1 then
                 data[vi] = c_dirt
@@ -117,7 +100,7 @@ minetest.register_on_generated(function(minp, maxp)
                 end
             end
 
-            ni = ni + 1
+            node_index = node_index + 1
 
             vi = vi + 1
         end
@@ -126,9 +109,7 @@ minetest.register_on_generated(function(minp, maxp)
 
 
     vm:set_data(data)
-
-    vm:set_lighting({day=15,night=0}, minp, maxp)
-
+    vm:set_lighting({day=15,night=10}, minp, maxp)
     vm:write_to_map()
         
         
