43ff51d3f7d417746029f751f93ff840f5da384a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-07 20:15:35 -0500

    Optimize with blocking statement

--- mods/player_mechanics/fall_damage.lua
+++ mods/player_mechanics/fall_damage.lua
@@ -51,89 +51,76 @@ local stack_name
 
 local function calc_fall_damage(player,hp_change)
 
-    if cancel_fall_damage(player) then
-        return
-    else
-        inv = player:get_inventory()
-        stack = inv:get_stack("armor_feet", 1)
-        stack_name = stack:get_name()
+    if cancel_fall_damage(player) then return end
+    
+    inv = player:get_inventory()
+    stack = inv:get_stack("armor_feet", 1)
+    stack_name = stack:get_name()
 
-        if stack_name ~= "" then
+    if stack_name ~= "" then
 
-            absorption = get_item_group( stack_name, "armor_level" ) * 2
-            
-            local wear_level = ( ( 9 - get_item_group( stack_name, "armor_level" ) ) * 8 ) * ( 5 - get_item_group( stack_name, "armor_type" ) ) * math_abs( hp_change )
-            
-            stack:add_wear( wear_level )
-            
-            inv:set_stack( "armor_feet", 1, stack )
-            
-            local new_stack = inv:get_stack( "armor_feet", 1 ):get_name()
+        absorption = get_item_group( stack_name, "armor_level" ) * 2
+        
+        local wear_level = ( ( 9 - get_item_group( stack_name, "armor_level" ) ) * 8 ) * ( 5 - get_item_group( stack_name, "armor_type" ) ) * math_abs( hp_change )
+        
+        stack:add_wear( wear_level )
+        
+        inv:set_stack( "armor_feet", 1, stack )
+        
+        local new_stack = inv:get_stack( "armor_feet", 1 ):get_name()
 
-            if new_stack == "" then
-                sound_play(
-                    "armor_break",
-                    {
-                        to_player = player:get_player_name(),
-                        gain = 1,
-                        pitch = math_random( 80, 100 ) / 100
-                    }
-                )
-                recalculate_armor( player )
-                set_armor_gui( player )
+        if new_stack == "" then
+            sound_play(
+                "armor_break",
+                {
+                    to_player = player:get_player_name(),
+                    gain = 1,
+                    pitch = math_random( 80, 100 ) / 100
+                }
+            )
+            recalculate_armor( player )
+            set_armor_gui( player )
 
-                -- Do particles as well
-            elseif get_item_group(new_stack,"boots") > 0 then
-                pos = player:get_pos()
-                add_particlespawner({
-                    amount = 30,
-                    time = 0.00001,
-                    minpos = vec_new( pos.x - 0.5, pos.y + 0.1, pos.z - 0.5 ),
-                    maxpos = vec_new( pos.x + 0.5, pos.y + 0.1, pos.z + 0.5 ),
-                    minvel = vec_new( -0.5, 1, -0.5 ),
-                    maxvel = vec_new( 0.5, 2, 0.5 ),
-                    minacc = vec_new( 0, -9.81, 1 ),
-                    maxacc = vec_new( 0, -9.81, 1 ),
-                    minexptime = 0.5,
-                    maxexptime = 1.5,
-                    minsize = 0,
-                    maxsize = 0,
-                    --attached = player,
-                    collisiondetection = true,
-                    collision_removal = true,
-                    vertical = false,
-                    node = {name= stack_name.."particletexture"},
-                    --texture = "eat_particles_1.png"
-                })
-                sound_play(
-                    "armor_fall_damage",
-                    {
-                        object = player,
-                        gain = 1.0,
-                        max_hear_distance = 60,
-                        pitch = math_random( 80, 100 ) / 100
-                    }
-                )
-            end
+            -- Do particles as well
+        elseif get_item_group(new_stack,"boots") > 0 then
+            pos = player:get_pos()
+            add_particlespawner({
+                amount = 30,
+                time = 0.00001,
+                minpos = vec_new( pos.x - 0.5, pos.y + 0.1, pos.z - 0.5 ),
+                maxpos = vec_new( pos.x + 0.5, pos.y + 0.1, pos.z + 0.5 ),
+                minvel = vec_new( -0.5, 1, -0.5 ),
+                maxvel = vec_new( 0.5, 2, 0.5 ),
+                minacc = vec_new( 0, -9.81, 1 ),
+                maxacc = vec_new( 0, -9.81, 1 ),
+                minexptime = 0.5,
+                maxexptime = 1.5,
+                minsize = 0,
+                maxsize = 0,
+                --attached = player,
+                collisiondetection = true,
+                collision_removal = true,
+                vertical = false,
+                node = {name= stack_name.."particletexture"},
+                --texture = "eat_particles_1.png"
+            })
+            sound_play(
+                "armor_fall_damage",
+                {
+                    object = player,
+                    gain = 1.0,
+                    max_hear_distance = 60,
+                    pitch = math_random( 80, 100 ) / 100
+                }
+            )
+        end
 
-            hp_change = hp_change + absorption
+        hp_change = hp_change + absorption
 
-            if hp_change >= 0 then
-                hp_change = 0
-            else
-                player:set_hp( player:get_hp() + hp_change, { reason = "correction" } )
-                sound_play(
-                    "hurt",
-                    {
-                        object = player,
-                        gain = 1.0,
-                        max_hear_distance = 60,
-                        pitch = math_random( 80, 100 ) / 100
-                    }
-                )
-            end
+        if hp_change >= 0 then
+            hp_change = 0
         else
-            player:set_hp( player:get_hp() + hp_change,  { reason = "correction" } )
+            player:set_hp( player:get_hp() + hp_change, { reason = "correction" } )
             sound_play(
                 "hurt",
                 {
@@ -144,6 +131,17 @@ local function calc_fall_damage(player,hp_change)
                 }
             )
         end
+    else
+        player:set_hp( player:get_hp() + hp_change,  { reason = "correction" } )
+        sound_play(
+            "hurt",
+            {
+                object = player,
+                gain = 1.0,
+                max_hear_distance = 60,
+                pitch = math_random( 80, 100 ) / 100
+            }
+        )
     end
 end
 
