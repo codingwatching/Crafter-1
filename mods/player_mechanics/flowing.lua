43b37a7c2e77bee72bb3e9c7cd0b8ac9612f3a20
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-07 22:43:12 -0500

    Inversion optimization

--- mods/player_mechanics/flowing.lua
+++ mods/player_mechanics/flowing.lua
@@ -10,42 +10,46 @@ local name
 minetest.register_globalstep(function()
 
     for _,player in ipairs( minetest.get_connected_players() ) do
-        flow_dir = flow(player:get_pos())
+
+        flow_dir = flow( player:get_pos() )
+
         name = player:get_player_name()
-        if flow_dir then
-            --buffer continuation
-            if pool[name] then
-                c_flow = pool[name]
-                acceleration = nil
-                if c_flow.x ~= 0 then
-                    acceleration = vector.new(c_flow.x,0,0)
-                elseif c_flow.z ~= 0 then
-                    acceleration = vector.new(0,0,c_flow.z)
-                end
-                acceleration = vector.multiply(acceleration, 0.075)
-                player:add_velocity(acceleration)
 
-                newvel = player:get_velocity()
+        if not flow_dir then
+            pool[name] = nil
+            return
+        end
 
-                if newvel.x ~= 0 or newvel.z ~= 0 then
-                    return
-                else
-                    pool[name] = nil
-                end
+        -- Buffer continuation
+        if pool[name] then
+            c_flow = pool[name]
+            acceleration = nil
+            if c_flow.x ~= 0 then
+                acceleration = vector.new(c_flow.x,0,0)
+            elseif c_flow.z ~= 0 then
+                acceleration = vector.new(0,0,c_flow.z)
+            end
+            acceleration = vector.multiply(acceleration, 0.075)
+            player:add_velocity(acceleration)
+
+            newvel = player:get_velocity()
+
+            if newvel.x ~= 0 or newvel.z ~= 0 then
+                return
             else
-                flow_dir = vector.multiply(flow_dir,10)
-                acceleration = nil
-                if flow_dir.x ~= 0 then
-                    acceleration = vector.new(flow_dir.x,0,0)
-                elseif flow_dir.z ~= 0 then
-                    acceleration = vector.new(0,0,flow_dir.z)
-                end
-                acceleration = vector.multiply(acceleration, 0.075)
-                player:add_velocity(acceleration)
-                pool[name] = flow_dir
+                pool[name] = nil
             end
         else
-            pool[name] = nil
+            flow_dir = vector.multiply(flow_dir,10)
+            acceleration = nil
+            if flow_dir.x ~= 0 then
+                acceleration = vector.new(flow_dir.x,0,0)
+            elseif flow_dir.z ~= 0 then
+                acceleration = vector.new(0,0,flow_dir.z)
+            end
+            acceleration = vector.multiply(acceleration, 0.075)
+            player:add_velocity(acceleration)
+            pool[name] = flow_dir
         end
     end
 end)
