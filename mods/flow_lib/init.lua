4f931d444c6eafcbc2893f018543347ec15d67e6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-11 17:44:39 -0500

    Import more functions

--- mods/flow_lib/init.lua
+++ mods/flow_lib/init.lua
@@ -1,15 +1,34 @@
--- This is from: https://github.com/appgurueu/modlib/blob/master/minetest/liquid.lua
-
 --[[ License information
 Copyright 2019 - 2021 Lars Mueller alias LMD or appguru(eu)
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+This has been modified by jordan4ibanez
 ]]
 
--- Localize globals
-local math, minetest, modlib, pairs = math, minetest, modlib, pairs
+-- https://github.com/appgurueu/modlib/blob/master/vector.lua
 
+local function apply(v, f, ...)
+	local new_vector = {}
+	for key, value in pairs(v) do
+		new_vector[key] = f(value, ...)
+	end
+	return vector.new(new_vector)
+end
+
+local function combinator(f)
+	return function(v, w)
+		return combine(v, w, f)
+	end, function(v, ...)
+		return apply(v, f, ...)
+	end
+end
+
+local divide, divide_scalar = combinator(function(v, w) return v / w end)
+
+
+-- https://github.com/appgurueu/modlib/blob/master/minetest/liquid.lua
 local liquid_level_max = 8
 --+ Calculates the corner levels of a flowingliquid node
 --> 4 corner levels from -0.5 to 0.5 as list of `modlib.vector`
