eee61b97b73029675c8476038c1899ac28050f86
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-11 17:49:31 -0500

    Make this thing more understandable

--- mods/flow_lib/init.lua
+++ mods/flow_lib/init.lua
@@ -4,7 +4,7 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-This has been modified by jordan4ibanez
+This has been heavily modified by jordan4ibanez
 ]]
 
 -- https://github.com/appgurueu/modlib/blob/master/vector.lua
@@ -24,8 +24,17 @@ local function combinator(f)
 		return apply(v, f, ...)
 	end
 end
-local subtract, subtract_scalar = combinator(function(v, w) return v - w end)
-local divide, divide_scalar = combinator(function(v, w) return v / w end)
+-- NOTE: These return tuples
+local subtract, subtract_scalar = combinator(
+    function(v, w)
+        return v - w
+    end
+)
+local divide, divide_scalar = combinator(
+    function(v, w)
+        return v / w
+    end
+)
 
 
 -- https://github.com/appgurueu/modlib/blob/master/minetest/liquid.lua
