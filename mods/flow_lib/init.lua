11c4ba8a603ef8ca3340bbee91738d133477c9ec
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-11 17:39:40 -0500

    Remove environmental modification

--- mods/flow_lib/init.lua
+++ mods/flow_lib/init.lua
@@ -10,10 +10,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 -- Localize globals
 local math, minetest, modlib, pairs = math, minetest, modlib, pairs
 
--- Set environment
-local _ENV = ...
-setfenv(1, _ENV)
-
 local liquid_level_max = 8
 --+ Calculates the corner levels of a flowingliquid node
 --> 4 corner levels from -0.5 to 0.5 as list of `modlib.vector`
