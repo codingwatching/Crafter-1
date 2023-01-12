34e48fc7b220d873fc833b3cda4629fc715d7bab
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-11 22:24:18 -0500

    Fix player being pushed around on random things

--- mods/player_mechanics/flowing.lua
+++ mods/player_mechanics/flowing.lua
@@ -1,6 +1,7 @@
 local ipairs                = ipairs
 local vec_multiply          = vector.multiply
 local get_connected_players = minetest.get_connected_players
+local get_node              = minetest.get_node
 
 local pool = {}
 local c_flow
@@ -8,15 +9,24 @@ local acceleration
 local newvel
 local flow_dir
 local name
+local pos
+local current_node
 
 -- TODO: try to force the player smoothly to move in the direction
 minetest.register_globalstep(function()
 
     for _,player in ipairs( get_connected_players() ) do
 
-        flow_dir = get_liquid_flow_direction( player:get_pos() )
-
+        pos = player:get_pos()
+        current_node = get_node(pos).name
         name = player:get_player_name()
+
+        if current_node ~= "main:water" and current_node ~= "main:waterflow" then
+            pool[name] = nil
+            goto continue
+        end
+
+        flow_dir = get_liquid_flow_direction( pos )
 
         if not flow_dir then
             pool[name] = nil
