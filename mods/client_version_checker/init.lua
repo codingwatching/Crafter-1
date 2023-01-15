03d029cb79ab469ceed3fd519ed4eddca63ec444
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-14 22:14:04 -0500

    Time to overhaul this mess

--- mods/client_version_checker/init.lua
+++ mods/client_version_checker/init.lua
@@ -1,11 +1,12 @@
+-- What the heck is this
 minetest.register_node("client_version_checker:this_is_the_signature_of_crafter00111010010001000011110000110011",{})
 
 local client_versions = {}
 local client_version_channels = {}
 
---I needed to add in a dev cycle to adopt for a decimal place error
+-- Storing a semantic versioning in a table like: {"alpha", 0.071}. This is serialized.
 local current_development_cycle = "alpha"
-local current_version = 0.07000
+local current_version = 0.071
 
 minetest.register_on_joinplayer(function(player)
     local name = player:get_player_name()
@@ -13,13 +14,15 @@ minetest.register_on_joinplayer(function(player)
 end)
 
 minetest.register_on_modchannel_message(function(channel_name, sender, message)
+
     local channel_decyphered = channel_name:gsub(sender,"")
+
     if channel_decyphered == ":client_version_channel" then
         local version = tonumber(message)
         if type(version) ~= "number" then
             minetest.chat_send_player(sender, minetest.colorize("yellow", "Please do not try to crash the server."))
-            for i = 1,5 do
-                minetest.log("warning", sender.." tried to crash the server!")
+            for _ = 1,5 do
+                minetest.log("warning", sender .. " tried to crash the server!")
             end
         elseif type(version) == "number" then
             if current_development_cycle == "alpha" and version > 0.0999 then
