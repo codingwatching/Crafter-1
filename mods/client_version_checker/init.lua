31c469485a34ddb7baeb0be45bbfc92455ce02ad
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-14 23:16:58 -0500

    Rescope everything

--- mods/client_version_checker/init.lua
+++ mods/client_version_checker/init.lua
@@ -2,7 +2,7 @@ local type = type
 local tostring = tostring
 
 -- What the heck is this
-minetest.register_node(":crafterclient_version_checker:this_is_a_hack_to_not_crash_other_servers",{})
+minetest.register_node(":crafter_client_version_checker:this_is_a_hack_to_not_crash_other_servers",{})
 
 local client_versions = {}
 local client_version_channels = {}
@@ -20,6 +20,28 @@ end)
 
 -- TODO: inform the person that they need the client mod 10 seconds after they join if they're not in the version channel
 
+-- This person is a piece of crap and is trying to crash an opensource game server, let's kick their ass out
+local function trying_to_crash_server(sender)
+    minetest.kick_player(sender, "Stop trying to crash the server.")
+    minetest.log("action", sender .. " tried to crash the server.")
+end
+
+-- Something that absolutely isn't supposed to happen, happened
+local function a_serious_error(sender)
+    minetest.kick_player(sender, "Something has gone seriously wrong, please check your client mod version.")
+    minetest.log("action", sender .. " had a serious glitch occur with their client mod.")
+end
+
+-- This person has an outdated or future client mod, let them know in a kick message
+local function wrong_client_version(sender, reported_development_cycle, reported_version)
+    minetest.kick_player(sender,
+        "You have the wrong client version.\n" ..
+        "Your version: " .. tostring(reported_version) .. " " .. reported_development_cycle .. "\n" ..
+        "Server version: " .. tostring(current_version) .. " " .. current_development_cycle .. "\n" ..
+        "To install the correct version, please go to:\n" .. current_client_link
+    )
+end
+
 -- TODO: all of these return ends needs to jump to a section that kicks them with the correct client mod link
 minetest.register_on_modchannel_message(function(channel_name, sender, message)
 
@@ -28,52 +50,47 @@ minetest.register_on_modchannel_message(function(channel_name, sender, message)
     if channel_decyphered ~= ":client_version_channel" then return end
 
     -- I don't know why this would ever happen but check anyways
-    if not message then goto a_serious_error_occured end
+    if not message then
+        a_serious_error(sender)
+        return
+    end
 
     local version_info = minetest.deserialize(message)
 
     -- Player tried to do something weird to crash the server
-    if not version_info then goto youre_an_asshole end
+    if not version_info then
+        trying_to_crash_server(sender)
+        return
+    end
 
     -- Random data, tried to crash the server
-    if not type(version_info) == "table" then goto youre_an_asshole end
+    if not type(version_info) == "table" then
+        trying_to_crash_server(sender)
+        return
+    end
 
     -- Not the right amount of data, tried to crash the server
-    if #version_info ~= 2 then goto youre_an_asshole end
+    if #version_info ~= 2 then
+        trying_to_crash_server(sender)
+        return
+    end
 
     -- Not the right type of data, tried to crash the server
-    if type(version_info[1]) ~= "string" or type(version_info[2]) ~= "number" then goto youre_an_asshole end
+    if type(version_info[1]) ~= "string" or type(version_info[2]) ~= "number" then
+        trying_to_crash_server(sender)
+        return
+    end
 
     -- We know everything is okay and this person is just trying to play, let's continue
     local reported_development_cycle = version_info[1]
     local reported_version = version_info[2]
 
     -- Unfortunately, the client info does not match, kick them and tell them
-    if reported_development_cycle ~= current_development_cycle or reported_version ~= current_version then goto wrong_client_version end
+    if reported_development_cycle ~= current_development_cycle or reported_version ~= current_version then
+        wrong_client_version(sender, reported_development_cycle, reported_version)
+        return
+    end
 
-
-    -- This person is a piece of crap and is trying to crash an opensource game server, let's kick their ass out
-    ::youre_an_asshole::
-
-    minetest.kick_player(sender, "Stop trying to crash the server.")
-    minetest.log("action", sender .. " tried to crash the server.")
-
-    do return end
-
-    -- This person has an outdated or future client mod, let them know in a kick message
-    ::wrong_client_version::
-
-    minetest.kick_player(sender,
-        "You have the wrong client version.\n" ..
-        "Your version: " .. tostring(reported_version) .. " " .. reported_development_cycle .. "\n" ..
-        "Server version: " .. tostring(current_version) .. " " .. current_development_cycle .. "\n" ..
-        "To install the correct version, please go to:\n" .. current_client_link
-    )
-
-    -- Something that absolutely isn't supposed to happen, happened
-    ::a_serious_error_occured::
-
-    minetest.kick_player(sender, "Something has gone seriously wrong, please check your client mod version.")
-    minetest.log("action", sender .. " had a serious glitch occur with their client mod.")
+    -- Everything checked out
 end)
 
