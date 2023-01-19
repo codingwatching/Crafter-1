67eee9987671cfd1c6a88992f0bfe71292fa8713
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-18 23:30:11 -0500

    Micro opts

--- mods/fire/init.lua
+++ mods/fire/init.lua
@@ -1,7 +1,18 @@
-local ipairs = ipairs
+local get_node_timer = minetest.get_node_timer
+local get_node = minetest.get_node
+local set_node = minetest.set_node
+local find_node_near = minetest.find_node_near
+local sound_play = minetest.sound_play
+local registered_nodes = minetest.registered_nodes
+local item_place = minetest.item_place
+local mod_channel_join = minetest.mod_channel_join
+local remove_node = minetest.remove_node
+local add_entity = minetest.add_entity
+local after = minetest.after
+local log = minetest.log
 
 local function start_fire_timer(pos)
-    minetest.get_node_timer(pos):start(math.random(1,7))
+    get_node_timer(pos):start(math.random(1,7))
 end
 
 minetest.register_node("fire:fire", {
@@ -27,10 +38,10 @@ minetest.register_node("fire:fire", {
     light_source = 11,
     on_construct = function(pos)
 
-        local under = minetest.get_node( vector.new( pos.x, pos.y - 1, pos.z ) ).name
+        local under = get_node( vector.new( pos.x, pos.y - 1, pos.z ) ).name
         -- Creates a nether portal
         if under == "nether:obsidian" then
-            minetest.remove_node(pos)
+            remove_node(pos)
             create_nether_portal(pos)
         -- Fire lasts forever on netherrack, as a side effect, you can also make fire art with this :)
         elseif under ~= "nether:netherrack" then
@@ -39,11 +50,11 @@ minetest.register_node("fire:fire", {
     end,
     on_timer = function(pos)
 
-        local fire_spread_position = minetest.find_node_near(pos, 1, {"group:flammable"})
+        local fire_spread_position = find_node_near(pos, 1, {"group:flammable"})
 
         -- Reduce the amount of serverwide fires that can happen from stagnant fire nodes in air
         if not fire_spread_position then
-            minetest.remove_node(pos)
+            remove_node(pos)
             return
         end
 
@@ -52,7 +63,7 @@ minetest.register_node("fire:fire", {
             -- huge server wide fires that can happen
             if math.random() > 0.25 then
 
-                minetest.set_node( fire_spread_position, { name = "fire:fire" } )
+                set_node( fire_spread_position, { name = "fire:fire" } )
                 start_fire_timer(fire_spread_position)
             end
         end
@@ -73,28 +84,28 @@ minetest.register_tool("fire:flint_and_steel", {
 
         local sneak = placer:get_player_control().sneak
 
-        local nodedef = minetest.registered_nodes[minetest.get_node(pointed_thing.under).name]
+        local nodedef = registered_nodes[get_node(pointed_thing.under).name]
 
-        if not sneak and nodedef.on_rightclick then return minetest.item_place(itemstack, placer, pointed_thing) end
+        if not sneak and nodedef.on_rightclick then return item_place(itemstack, placer, pointed_thing) end
 
         -- Can't make fire in the aether
         if pointed_thing.above.y >= 20000 then
-            minetest.sound_play( "flint_failed", {
+            sound_play( "flint_failed", {
                 pos = pointed_thing.above,
                 pitch = math.random( 75, 95 ) / 100
             })
             return
         end
 
-        if minetest.get_node(pointed_thing.above).name ~= "air" then
-            minetest.sound_play( "flint_failed", {
+        if get_node(pointed_thing.above).name ~= "air" then
+            sound_play( "flint_failed", {
                 pos = pointed_thing.above
             })
             return
         end
 
-        minetest.add_node( pointed_thing.above, { name = "fire:fire" } )
-        minetest.sound_play( "flint_and_steel", {
+        set_node( pointed_thing.above, { name = "fire:fire" } )
+        sound_play( "flint_and_steel", {
             pos = pointed_thing.above
         })
 
@@ -243,7 +254,7 @@ local fire_channels = {}
 minetest.register_on_joinplayer(function(player)
 
     local player_name = player:get_player_name()
-    fire_channels[player_name] = minetest.mod_channel_join(player_name..":fire_state")
+    fire_channels[player_name] = mod_channel_join(player_name..":fire_state")
 
     local metatable = getmetatable(player)
 
@@ -260,11 +271,11 @@ minetest.register_on_joinplayer(function(player)
         -- Player is being lit on fire by a mod
         if state then
 
-            local fire_entity = minetest.add_entity( player:get_pos(), "fire:fire" )
+            local fire_entity = add_entity( player:get_pos(), "fire:fire" )
 
             -- A serious glitch has occured, log and keep the server running
             if not fire_entity then
-                minetest.log("action", "A serious error has occurred attaching an entity to " .. name .. "!")
+                log("action", "A serious error has occurred attaching an entity to " .. name .. "!")
                 return
             end
 
@@ -284,9 +295,9 @@ minetest.register_on_joinplayer(function(player)
                 fire_entity:remove()
             end
             fire_channels[name]:send_all("0")
-            
+
             if not silent_extinguish then
-                minetest.sound_play( "fire_extinguish", {
+                sound_play( "fire_extinguish", {
                     object = player,
                     gain = 0.3,
                     pitch = math.random( 80, 100 ) / 100
@@ -307,7 +318,7 @@ minetest.register_on_joinplayer(function(player)
 
     player:set_fire_state(false, true)
 
-    minetest.after(2.1, function()
+    after(2.1, function()
         player:set_fire_state(true)
     end)
 
@@ -315,7 +326,6 @@ end)
 
 --[[
 
--- TODO: this is a deprectated function remove this
 function is_entity_on_fire(object)
     return false
 end
