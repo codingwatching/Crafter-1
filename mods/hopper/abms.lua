4b632eeacaa76572d9c7e64d608f8c895e8f54e7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-21 02:16:28 -0500

    Make this easier to read

--- mods/hopper/abms.lua
+++ mods/hopper/abms.lua
@@ -69,12 +69,15 @@ local directions = {
 }
 
 local bottomdir = function(facedir)
-    return ({[0]={x=0, y=-1, z=0},
-        {x=0, y=0, z=-1},
-        {x=0, y=0, z=1},
-        {x=-1, y=0, z=0},
-        {x=1, y=0, z=0},
-        {x=0, y=1, z=0}})[math.floor(facedir/4)]
+    return (
+    { [0] =
+        { x = 0, y = -1, z = 0 },
+        { x = 0, y = 0, z = -1 },
+        { x = 0, y = 0, z = 1 },
+        { x = -1, y = 0, z = 0 },
+        { x = 1, y = 0, z = 0 },
+        { x = 0, y = 1, z = 0 }
+    })[ math.floor( facedir / 4 ) ]
 end
 
 -- hopper workings
