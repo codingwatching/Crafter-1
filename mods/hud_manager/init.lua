f911ce5d226e033ed7e56835550c826f5f35497b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-23 22:47:34 -0500

    Turn hud manager into method for player

--- mods/hud_manager/init.lua
+++ mods/hud_manager/init.lua
@@ -1,59 +1,51 @@
-local minetest = minetest
+local getmetatable = getmetatable
 
-local player_huds = {} -- the list of players hud lists (3d array)
-hud_manager = {}       -- hud manager class
+local player_huds = {}
 
--- terminate the player's list on leave
-minetest.register_on_leaveplayer(function(player)
+-- Set up initial data container
+minetest.register_on_joinplayer(function(player)
+    local metatable = getmetatable(player)
+    player_huds[name] = {}
     local name = player:get_player_name()
-    player_huds[name] = nil
+    -- Heap pointer object becomes bound to this player's methods
+    local data_container = player_huds[name]
+
+    function metatable:add_hud( hud_name, def )
+        -- The value gets stored as so: table["key"] = 1 -- or whatever the number is
+        data_container[hud_name] = player:hud_add({
+            hud_elem_type = def.hud_elem_type,
+            position      = def.position,
+            text          = def.text,
+            number        = def.number,
+            direction     = def.direction,
+            size          = def.size,
+            offset        = def.offset,
+        })
+    end
+
+    function metatable:remove_hud(hud_name)
+        local gotten_id = data_container[hud_name]
+        if not gotten_id then return end
+        player:hud_remove(gotten_id)
+        data_container[hud_name] = nil
+    end
+
+    function metatable:change_hud(hud_name, data)
+        local gotten_id = data_container[hud_name]
+        if not gotten_id then return end
+        player:hud_change( data_container[hud_name], data.element, data.data )
+    end
+
+    function metatable:hud_exists(hud_name)
+        return data_container[hud_name] ~= nil
+    end
+
+    function metatable:delete_hud_data_container()
+        player_huds[name] = nil
+    end
 end)
 
--- create instance of new hud
-hud_manager.add_hud = function(player,hud_name,def)
-    local name = player:get_player_name()
-    local local_hud = player:hud_add({
-        hud_elem_type = def.hud_elem_type,
-        position      = def.position,
-        text          = def.text,
-        number        = def.number,
-        direction     = def.direction,
-        size          = def.size,
-        offset        = def.offset,
-    })
-    -- create new 3d array here
-    -- depends.txt is not needed
-    -- with it here
-    if not player_huds[name] then
-        player_huds[name] = {}
-    end
-
-    player_huds[name][hud_name] = local_hud
-end
-
--- delete instance of hud
-hud_manager.remove_hud = function(player,hud_name)
-    local name = player:get_player_name()
-    if player_huds[name] and player_huds[name][hud_name] then
-        player:hud_remove(player_huds[name][hud_name])
-        player_huds[name][hud_name] = nil
-    end
-end
-
--- change element of hud
-hud_manager.change_hud = function(data)
-    local name = data.player:get_player_name()
-    if player_huds[name] and player_huds[name][data.hud_name] then
-        data.player:hud_change(player_huds[name][data.hud_name], data.element, data.data)
-    end
-end
-
--- gets if hud exists
-hud_manager.hud_exists = function(player,hud_name)
-    local name = player:get_player_name()
-    if player_huds[name] and player_huds[name][hud_name] then
-        return(true)
-    else
-        return(false)
-    end
-end
+-- Remove the player's data container
+minetest.register_on_leaveplayer(function(player)
+    player:delete_hud_data_container()
+end)
