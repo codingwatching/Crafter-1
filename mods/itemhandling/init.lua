fb357dd8f81bd3231649ec07f17bb5bc1f878f85
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-25 00:16:18 -0500

    Inversions everywhere

--- mods/itemhandling/init.lua
+++ mods/itemhandling/init.lua
@@ -21,46 +21,56 @@ local diff
 local inv
 local entity
 local tick = false
+local data_container
+-- TODO: switch this to DTIME
 local function magnet(player)
-    --don't magnetize to dead players
+
+    -- Don't magnetize to dead players
     name = player:get_player_name()
-    if player:get_hp() > 0 then
-        pos = player:get_pos()
-        inv = player:get_inventory()
-        if tick == true and pool[name] > 0 then
-            minetest.sound_play("pickup", {
-                to_player = player:get_player_name(),
-                gain = 0.4,
-                pitch = math.random(60,100)/100
-            })
-            if pool[name] > 6 then
-                pool[name] = 6
-            else
-                pool[name] = pool[name] - 1
-            end
-        end
-        --radial detection
-        for _,object in ipairs(minetest.get_objects_inside_radius({x=pos.x,y=pos.y+0.5,z=pos.z}, 2)) do
-            if not object:is_player() then
-                entity = object:get_luaentity()
-                if entity.name == "__builtin:item" and entity.collectable == true and object:get_luaentity().collected == false then
-                    pos2 = object:get_pos()
-                    diff = vector.subtract(pos2,pos).y
-                    if diff >= 0 and inv:room_for_item("main", entity.itemstring) then
-                        pool[name] = pool[name] + 1    
-                        inv:add_item("main", entity.itemstring)
-                        entity.collector = player:get_player_name()
-                        entity.collected = true                            
-                        
-                    end
-                elseif not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "experience:orb" then
-                        entity.collector = player:get_player_name()
-                        entity.collected = true
-                end
-            end
-        end
+    data_container = pool[name]
+    if player:get_hp() <= 0 then
+        data_container = 0
+        return
+    end
+
+    pos = player:get_pos()
+    inv = player:get_inventory()
+
+    if not tick or pool[name] <= 0 then goto skip_playing_sound end
+
+    minetest.sound_play("pickup", {
+        to_player = player:get_player_name(),
+        gain = 0.4,
+        pitch = math.random(60,100)/100
+    })
+
+    if data_container > 6 then
+        data_container = 6
     else
-        pool[name] = 0
+        data_container = data_container - 1
+    end
+
+    ::skip_playing_sound::
+
+    --radial detection
+    for _,object in ipairs(minetest.get_objects_inside_radius({x=pos.x,y=pos.y+0.5,z=pos.z}, 2)) do
+        if not object:is_player() then
+            entity = object:get_luaentity()
+            if entity.name == "__builtin:item" and entity.collectable == true and object:get_luaentity().collected == false then
+                pos2 = object:get_pos()
+                diff = vector.subtract(pos2,pos).y
+                if diff >= 0 and inv:room_for_item("main", entity.itemstring) then
+                    pool[name] = pool[name] + 1    
+                    inv:add_item("main", entity.itemstring)
+                    entity.collector = player:get_player_name()
+                    entity.collected = true                            
+                    
+                end
+            elseif not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "experience:orb" then
+                    entity.collector = player:get_player_name()
+                    entity.collected = true
+            end
+        end
     end
 end
 
