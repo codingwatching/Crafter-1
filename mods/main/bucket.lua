71b0adfb6de2390e04840c90917dfecd732faeed
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-25 21:33:02 -0500

    Code reduction

--- mods/main/bucket.lua
+++ mods/main/bucket.lua
@@ -43,22 +43,48 @@ minetest.register_craftitem("main:bucket", {
 local function place_function(itemstack, placer)
     local pos = bucket_raycast(placer)
     if not pos then return end
-    print(itemstack:get_name())
+    local bucket_type = itemstack:get_name():gsub("bucket_", "")
     local pos_under = pos.under
     local pos_above = pos.above
     local node_under = minetest.get_node(pos_under).name
     local node_above = minetest.get_node(pos_above).name
     local buildable_under = (minetest.registered_nodes[node_under].buildable_to == true)
     local buildable_above = (minetest.registered_nodes[node_above].buildable_to == true)
-    -- Set it to water
-    if buildable_under == true then
-        minetest.set_node(pos_under,{name="main:water"})
-        itemstack:replace(ItemStack("main:bucket"))
-        return(itemstack)
-    elseif buildable_above then
-        minetest.set_node(pos_above,{name="main:water"})
-        itemstack:replace(ItemStack("main:bucket"))
-        return(itemstack)
+
+    if bucket_type == "main:water" then
+        -- Set it to water
+        if buildable_under == true then
+            minetest.set_node(pos_under,{name="main:water"})
+            itemstack:replace(ItemStack("main:bucket"))
+            return(itemstack)
+        elseif buildable_above then
+            minetest.set_node(pos_above,{name="main:water"})
+            itemstack:replace(ItemStack("main:bucket"))
+            return(itemstack)
+        end
+    elseif bucket_type == "main:lava" then
+        -- Set it to lava
+        if buildable_under == true then
+            if pos_under.y < 20000 then
+                if pos_under.y > -10033 then
+                    minetest.add_node(pos_under,{name="main:lava"})
+                else
+                    minetest.add_node(pos_under,{name="nether:lava"})
+                end
+                itemstack:replace(ItemStack("main:bucket"))
+                return(itemstack)
+            end
+        elseif buildable_above then
+            if pos_above.y < 20000 then
+                if pos_above.y > -10033 then
+                    minetest.add_node(pos_above,{name="main:lava"})
+                else
+                    minetest.add_node(pos_above,{name="nether:lava"})
+                end
+                itemstack:replace(ItemStack("main:bucket"))
+                return(itemstack)
+            end
+        end
     end
 end
 
@@ -71,95 +97,10 @@ minetest.register_craftitem("main:bucket_water", {
     on_secondary_use = place_function,
 })
 
-
 minetest.register_craftitem("main:bucket_lava", {
     description = "Bucket of Lava",
     inventory_image = "bucket_lava.png",
     stack_max = 1,
-    --liquids_pointable = false,
-    on_place = function(itemstack, placer, pointed_thing)
-        if pointed_thing.under and minetest.get_node(pointed_thing.under).name == "tnt:tnt" then
-            minetest.remove_node(pointed_thing.under)
-            tnt(pointed_thing.under,7)
-            itemstack:replace(ItemStack("main:bucket"))
-            return(itemstack)
-        end
-        
-        local pos = bucket_raycast(placer)
-        
-        if not pos then
-            return
-        end
-        
-        local pos_under = pos.under
-        local pos_above = pos.above
-        
-        local node_under = minetest.get_node(pos_under).name
-        local node_above = minetest.get_node(pos_above).name
-        
-        local buildable_under = (minetest.registered_nodes[node_under].buildable_to == true)
-        local buildable_above = (minetest.registered_nodes[node_above].buildable_to == true)
-        
-        --set it to lava
-        if buildable_under == true then
-            if pos_under.y < 20000 then
-                if pos_under.y > -10033 then
-                    minetest.add_node(pos_under,{name="main:lava"})
-                else
-                    minetest.add_node(pos_under,{name="nether:lava"})
-                end
-                itemstack:replace(ItemStack("main:bucket"))
-                return(itemstack)
-            end
-        elseif buildable_above then
-            if pos_above.y < 20000 then
-                if pos_above.y > -10033 then
-                    minetest.add_node(pos_above,{name="main:lava"})
-                else
-                    minetest.add_node(pos_above,{name="nether:lava"})
-                end
-                itemstack:replace(ItemStack("main:bucket"))
-                return(itemstack)
-            end
-        end
-    end,
-    on_secondary_use = function(itemstack, user, pointed_thing)
-        local pos = bucket_raycast(user)
-        
-        if not pos then
-            return
-        end
-        
-        local pos_under = pos.under
-        local pos_above = pos.above
-        
-        local node_under = minetest.get_node(pos_under).name
-        local node_above = minetest.get_node(pos_above).name
-        
-        local buildable_under = (minetest.registered_nodes[node_under].buildable_to == true)
-        local buildable_above = (minetest.registered_nodes[node_above].buildable_to == true)
-        
-        --set it to lava
-        if buildable_under == true then
-            if pos_under.y < 20000 then
-                if pos_under.y > -10033 then
-                    minetest.add_node(pos_under,{name="main:lava"})
-                else
-                    minetest.add_node(pos_under,{name="nether:lava"})
-                end
-                itemstack:replace(ItemStack("main:bucket"))
-                return(itemstack)
-            end
-        elseif buildable_above then
-            if pos_above.y < 20000 then
-                if pos_above.y > -10033 then
-                    minetest.add_node(pos_above,{name="main:lava"})
-                else
-                    minetest.add_node(pos_above,{name="nether:lava"})
-                end
-                itemstack:replace(ItemStack("main:bucket"))
-                return(itemstack)
-            end
-        end
-    end,
+    on_place = place_function,
+    on_secondary_use = place_function,
 })
