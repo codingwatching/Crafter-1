fdd660ec8b98b971f991c8586408b3cd8594b31f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-26 22:48:32 -0500

    Micro opts

--- mods/main/lava_cooling.lua
+++ mods/main/lava_cooling.lua
@@ -1,5 +1,11 @@
+local find_node_near = minetest.find_node_near
+local set_node = minetest.set_node
+local get_meta = minetest.get_meta
+local math_floor = math.floor
+local vec_direction = vector.direction
+
 local function is_number_whole(input)
-    return math.floor(input) == input
+    return math_floor(input) == input
 end
 local function is_vec_whole(input)
     return is_number_whole(input.x) and is_number_whole(input.y) and is_number_whole(input.z)
@@ -12,11 +18,11 @@ minetest.register_abm({
     chance = 1,
     catch_up = false,
     action = function(pos)
-        local water = minetest.find_node_near(pos, 1, {"main:waterflow", "main:water"})
+        local water = find_node_near(pos, 1, {"main:waterflow", "main:water"})
         if not water then return end
         -- Only allow direct contact
-        if is_vec_whole(vector.direction(pos, water)) then
-            minetest.set_node(pos,{name="nether:obsidian"})
+        if is_vec_whole(vec_direction(pos, water)) then
+            set_node(pos,{name="nether:obsidian"})
         end
     end,
 })
@@ -30,12 +36,12 @@ minetest.register_abm({
     chance = 1,
     catch_up = false,
     action = function(pos)
-        local water = minetest.find_node_near(pos, 1, {"main:waterflow", "main:water"})
+        local water = find_node_near(pos, 1, {"main:waterflow", "main:water"})
         if not water then return end
         -- Only allow direct contact
-        if is_vec_whole(vector.direction(pos, water)) then
-            minetest.set_node(pos,{name="main:cobble"})
-            minetest.get_meta(pos):set_int("lava_cooled", 1)
+        if is_vec_whole(vec_direction(pos, water)) then
+            set_node(pos,{name="main:cobble"})
+            get_meta(pos):set_int("lava_cooled", 1)
         end
     end,
 })
