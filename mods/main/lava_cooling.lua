5bfa47bb1252cdfd01a661938765d717f2f8f182
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-26 22:17:26 -0500

    Fix an oversight

--- mods/main/lava_cooling.lua
+++ mods/main/lava_cooling.lua
@@ -1,47 +1,37 @@
 minetest.register_abm({
-    label = "Lava cooling",
+    label = "Lava source cooling",
     nodenames = {"main:lava"},
     neighbors = {"main:water", "main:waterflow"},
     interval = 3,
     chance = 1,
     catch_up = false,
     action = function(pos)
-
-        local lava = minetest.find_node_near(pos, 1, {"main:lava"})
-        local water = minetest.find_node_near(pos, 1, {"main:waterflow", "main:water"})
-
-        if lava and water then
-            local dir1 = vector.direction(pos, lava)
-            local dir2 = vector.direction(pos, water)
-            -- Only allow direct contact
-            if (math.abs(dir1.x) == 1 or math.abs(dir1.z) == 1) and (math.abs(dir2.x) == 1 or math.abs(dir2.z) == 1) then
-                minetest.set_node(pos,{name="nether:obsidian"})
-            end
+        local water = minetest.find_node_near(pos, 0.5, {"main:waterflow", "main:water"})
+        if not water then return end
+        local dir = vector.direction(pos, water)
+        -- Only allow direct contact
+        if math.abs(dir.x) == 1 or math.abs(dir.z) == 1 then
+            minetest.set_node(pos,{name="nether:obsidian"})
         end
     end,
 })
 
 -- This sets up the initial cobblestone generator, there is no on_flow function in the api at the current moment
 minetest.register_abm({
-    label = "Lava cooling",
+    label = "Lava flow cooling",
     nodenames = {"main:lavaflow"},
     neighbors = {"main:water", "main:waterflow"},
     interval = 3,
     chance = 1,
     catch_up = false,
     action = function(pos)
-
-        local lava = minetest.find_node_near(pos, 1, {"main:lavaflow"})
         local water = minetest.find_node_near(pos, 1, {"main:waterflow", "main:water"})
-
-        if lava and water then
-            local dir1 = vector.direction(pos, lava)
-            local dir2 = vector.direction(pos, water)
-            -- Only allow direct contact
-            if (math.abs(dir1.x) == 1 or math.abs(dir1.z) == 1) and (math.abs(dir2.x) == 1 or math.abs(dir2.z) == 1) then
-                minetest.set_node(pos,{name="main:cobble"})
-                minetest.get_meta(pos):set_int("lava_cooled", 1)
-            end
+        if not water then return end
+        local dir = vector.direction(pos, water)
+        -- Only allow direct contact
+        if math.abs(dir.x) == 1 or math.abs(dir.z) == 1 then
+            minetest.set_node(pos,{name="main:cobble"})
+            minetest.get_meta(pos):set_int("lava_cooled", 1)
         end
     end,
 })
