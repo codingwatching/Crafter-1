52921c4e4a41a2fc8ba76eaf359fbeca9d5f00e9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-26 23:34:48 -0500

    Overhaul saplings

--- mods/main/saplings.lua
+++ mods/main/saplings.lua
@@ -4,16 +4,21 @@ local get_node = minetest.get_node
 local set_node = minetest.set_node
 local place_schematic = minetest.place_schematic
 local register_node = minetest.register_node
-local register_abm = minetest.register_abm
-local registered_nodes = minetest.registered_nodes
-local item_place = minetest.item_place
-local sound_play = minetest.sound_play
+local remove_node = minetest.remove_node
+local get_node_timer = minetest.get_node_timer
+local dig_node = minetest.dig_node
 local vec_new = vector.new
 local math_random = math.random
 
+local function start_grow_timer(pos)
+    get_node_timer(pos):start(math_random(20,250))
+end
 
 local function sapling_grow(pos)
-    if get_node_light(pos, nil) < 10 then return end
+    if get_node_light(pos) < 10 then
+        start_grow_timer(pos)
+        return
+    end
 
     if get_item_group(get_node(vec_new(pos.x,pos.y-1,pos.z)).name, "soil") > 0 then
         local good_to_grow = true
@@ -25,16 +30,16 @@ local function sapling_grow(pos)
             end
         end
         if good_to_grow == true then
-            set_node(pos,{name="main:tree"})
-            local schemmy = math_random(1,2)
-            if schemmy == 1 then
+            remove_node(pos)
+            local schematic = math_random(1,2)
+            if schematic == 1 then
                 place_schematic(pos, tree_big,"0",nil,false,"place_center_x, place_center_z")
-            elseif schemmy == 2 then
+            elseif schematic == 2 then
                 place_schematic(pos, tree_small,"0",nil,false,"place_center_x, place_center_z")
             end
             --override leaves
             local max = 3
-            if schemmy == 2 then
+            if schematic == 2 then
                 max = 1
             end
             for i = 1,max do
@@ -52,7 +57,7 @@ register_node("main:sapling", {
     walkable = false,
     climbable = false,
     paramtype = "light",
-    is_ground_content = false,    
+    is_ground_content = false,
     tiles = {"sapling.png"},
     groups = {leaves = 1, plant = 1, axe = 1, hand = 0,instant=1, sapling=1, attached_node=1,flammable=1},
     sounds = main.dirtSound(),
@@ -62,48 +67,12 @@ register_node("main:sapling", {
         type = "fixed",
         fixed = {-4 / 16, -0.5, -4 / 16, 4 / 16, 7 / 16, 4 / 16}
     },
-    on_place =  function(itemstack, placer, pointed_thing)
-        if not pointed_thing.type == "node" then
+    on_construct = function(pos)
+        if get_item_group(get_node(vec_new(pos.x, pos.y - 1, pos.z)).name, "soil") <= 0 then
+            dig_node(pos)
             return
         end
-
-        local sneak = placer:get_player_control().sneak
-        local noddef = registered_nodes[get_node(pointed_thing.under).name]
-
-        if not sneak and noddef.on_rightclick then
-            item_place(itemstack, placer, pointed_thing)
-            return
-        end
-
-        local buildable = registered_nodes[get_node(pointed_thing.under).name].buildable_to
-        --replace buildable
-        if buildable and get_item_group(get_node(vec_new(pointed_thing.under.x,pointed_thing.under.y-1,pointed_thing.under.z)).name, "soil") > 0 then
-            return(item_place(itemstack, placer, pointed_thing))
-        end
-        local buildable = registered_nodes[get_node(pointed_thing.above).name].buildable_to
-        if buildable and get_item_group(get_node(vec_new(pointed_thing.above.x,pointed_thing.above.y-1,pointed_thing.above.z)).name, "soil") > 0 then
-            return(item_place(itemstack, placer, pointed_thing))
-        end
-        --place sapling
-        local pos = pointed_thing.above
-        if get_item_group(get_node(vec_new(pos.x,pos.y-1,pos.z)).name, "soil") > 0 and get_node(pointed_thing.above).name == "air" then
-            set_node(pointed_thing.above, {name="main:sapling"})
-            sound_play("dirt",{pos=pointed_thing.above})
-            itemstack:take_item(1)
-            return(itemstack)
-        end
+        start_grow_timer(pos)
     end,
-})
-
---growing abm for sapling
-register_abm({
-    label = "Tree Grow",
-    nodenames = {"group:sapling"},
-    neighbors = {"group:soil"},
-    interval = 6,
-    chance = 250,
-    catch_up = true,
-    action = function(pos)
-        sapling_grow(pos)
-    end,
+    on_timer = sapling_grow
 })
