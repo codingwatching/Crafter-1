6bf83ebf60a06473f1c1ded2fc116a5a4bdacdaa
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-26 19:11:10 -0500

    Tiny startup speed boost

--- mods/main/items.lua
+++ mods/main/items.lua
@@ -1,75 +1,76 @@
---item definition
+local register_food = minetest.register_food
+local register_craftitem = minetest.register_craftitem
 
-minetest.register_food("main:apple",{
+register_food("main:apple",{
     description = "Apple",
     texture = "apple.png",
     satiation=1,
     hunger=2,
 })
 
-minetest.register_food("main:sugar",{
+register_food("main:sugar",{
     description = "Sugar",
     texture = "sugar.png",
     satiation=1,
     hunger=1,
 })
 
-minetest.register_craftitem("main:stick", {
+register_craftitem("main:stick", {
     description = "Stick",
     inventory_image = "stick.png",
     groups = {stick = 1}
 })
-minetest.register_craftitem("main:paper", {
+register_craftitem("main:paper", {
     description = "Paper",
     inventory_image = "paper.png",
     groups = {paper = 1}
 })
 
-minetest.register_craftitem("main:coal", {
+register_craftitem("main:coal", {
     description = "Coal",
     inventory_image = "coal.png",
     groups = {coal = 1}
 })
 
-minetest.register_craftitem("main:charcoal", {
+register_craftitem("main:charcoal", {
     description = "Charcoal",
     inventory_image = "charcoal.png",
     groups = {coal = 1}
 })
 
-minetest.register_craftitem("main:iron", {
+register_craftitem("main:iron", {
     description = "Iron",
     inventory_image = "iron.png",
 })
 
-minetest.register_craftitem("main:gold", {
+register_craftitem("main:gold", {
     description = "Gold",
     inventory_image = "gold.png",
 })
 
-minetest.register_craftitem("main:diamond", {
+register_craftitem("main:diamond", {
     description = "Diamond",
     inventory_image = "diamond.png",
 })
-minetest.register_craftitem("main:flint", {
+register_craftitem("main:flint", {
     description = "Flint",
     inventory_image = "flint.png",
 })
 
-minetest.register_craftitem("main:lapis", {
+register_craftitem("main:lapis", {
     description = "Lapis Lazuli",
     inventory_image = "lapis.png",
 })
 
-minetest.register_craftitem("main:emerald", {
+register_craftitem("main:emerald", {
     description = "Emerald",
     inventory_image = "emerald.png",
 })
-minetest.register_craftitem("main:sapphire", {
+register_craftitem("main:sapphire", {
     description = "Sapphire",
     inventory_image = "sapphire.png",
 })
-minetest.register_craftitem("main:ruby", {
+register_craftitem("main:ruby", {
     description = "Ruby",
     inventory_image = "ruby.png",
 })
