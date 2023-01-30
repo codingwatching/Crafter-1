a7c150eb57ef86ef9d4cbad4091874b8d8c6af00
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-30 16:59:06 -0500

    Fix whitespace

--- mods/main/schematics.lua
+++ mods/main/schematics.lua
@@ -60,22 +60,22 @@ tree_small = {
     size = {x = 3, y = 5, z = 3},
     data = {
         -- The side of the bush, with the air on top
-        {name = "air"},            {name = "air"},            {name = "air"},
-        {name = "air"},            {name = "air"},            {name = "air"},
+        {name = "air"}, {name = "air"}, {name = "air"},
+        {name = "air"}, {name = "air"}, {name = "air"},
         {name = "main:leaves"}, {name = "main:leaves"}, {name = "main:leaves"}, -- lower layer
         {name = "main:leaves"}, {name = "main:leaves"}, {name = "main:leaves"}, -- middle layer
-        {name = "air"},            {name = "air"},            {name = "air"}, -- top layer
+        {name = "air"}, {name = "air"}, {name = "air"}, -- top layer
         -- The center of the bush, with stem at the base and a pointy leave 2 nodes above
-        {name = "air"},            {name = "main:tree"},       {name = "air"},
-        {name = "air"},            {name = "main:tree"},       {name = "air"},
+        {name = "air"}, {name = "main:tree"},       {name = "air"},
+        {name = "air"}, {name = "main:tree"},       {name = "air"},
         {name = "main:leaves"}, {name = "main:tree"},       {name = "main:leaves"}, -- lower layer
         {name = "main:leaves"}, {name = "main:leaves"},      {name = "main:leaves"}, -- middle layer
-        {name = "air"},            {name = "main:leaves"},    {name = "air"}, -- top layer
+        {name = "air"}, {name = "main:leaves"},    {name = "air"}, -- top layer
         -- The other side of the bush, same as first side
-        {name = "air"},            {name = "air"},            {name = "air"},
-        {name = "air"},            {name = "air"},            {name = "air"},
+        {name = "air"}, {name = "air"}, {name = "air"},
+        {name = "air"}, {name = "air"}, {name = "air"},
         {name = "main:leaves"}, {name = "main:leaves"}, {name = "main:leaves"}, -- lower layer
         {name = "main:leaves"}, {name = "main:leaves"}, {name = "main:leaves"}, -- middle layer
-        {name = "air"},            {name = "air"},            {name = "air"}, -- top layer
-        }
-        }
+        {name = "air"}, {name = "air"}, {name = "air"}, -- top layer
+    }
+}
