70c05bd26c6d8efda452bcbdfecd6f71af8289fe
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-31 18:04:30 -0500

    Shovel this garbage into that garbage

--- mods/mob/api/api_hook.lua
+++ mods/mob/api/api_hook.lua
@@ -21,11 +21,15 @@ mob.initial_properties = {
     static_save = false,
 }
 
---[[
+
 mob.is_mob = true
-mob.hp = definition.hp
-mob.max_speed = definition.max_speed
 mob.jump_timer = 0
+mob.movement_timer = 0
+mob.max_speed = definition.max_speed
+--[[
+
+mob.hp = definition.hp
+
 mob.head_bone = definition.head_bone
 -- mobs.create_head_functions(definition,mob)
 mob.debug_head_pos = definition.debug_head_pos
@@ -133,6 +137,109 @@ mobs.create_animation_functions(definition,mob)
 mobs.create_timer_functions(definition,mob)
 ]]
 
+function mob:manage_jump_timer(dtime)
+    if self.jump_timer > 0 then
+        self.jump_timer = self.jump_timer - dtime
+    end
+end
+
+function mob:move(dtime,moveresult)
+
+    self:manage_jump_timer(dtime)
+
+    self.movement_timer = self.movement_timer - dtime
+    
+    -- self:jump(moveresult)
+
+    -- self.swim(self,dtime)
+
+    -- Random direction state change
+    if self.movement_timer <= 0 and not self.following == true then
+        --print("changing direction")
+        self.movement_timer = math.random(2,7) + math.random()
+        self.direction = vector.new(math.random()*math.random(-1,1),0,math.random()*math.random(-1,1))
+        --local yaw = self.object:get_yaw() + dtime
+        self.speed = math.random(0,self.max_speed)
+        --self.object:set_yaw(yaw)
+    end
+
+    -- self.hurt_inside(self,dtime)
+
+    local currentvel = self.object:get_velocity()
+    local goal = vector.multiply(self.direction,self.speed)
+    local acceleration = vector.new(goal.x-currentvel.x,0,goal.z-currentvel.z)
+
+    --[[
+    if self.whip_turn then
+        self.whip_turn = self.whip_turn - dtime
+        if self.whip_turn <= 0 then
+            self.whip_turn = nil
+        end
+    else
+        acceleration = vector.multiply(acceleration, 0.05)
+    end
+    ]]
+
+    self.object:add_velocity(acceleration)
+end
+function mob:jump(moveresult)
+
+    if moveresult and moveresult.touching_ground and self.direction then
+
+        local pos = self.object:get_pos()
+        pos.y = pos.y+0.1
+
+        if self.path_data and #self.path_data > 0 then
+            --smart jump
+            local y = math.floor(pos.y+0.5)
+            local vel = self.object:get_velocity()
+            if y < self.path_data[1].y then
+                self.object:set_velocity(vector.new(vel.x,5,vel.z))
+            elseif self.path_data[2] and y < self.path_data[2].y then
+                self.object:set_velocity(vector.new(vel.x,5,vel.z))
+            elseif self.path_data[3] and y < self.path_data[3].y then
+                self.object:set_velocity(vector.new(vel.x,5,vel.z))
+            elseif ((vel.x == 0 and self.direction.x ~= 0) or (vel.z == 0 and self.direction.z ~= 0)) then
+                self.object:set_velocity(vector.new(vel.x,5,vel.z))
+            end
+        else
+            --assume collisionbox is even x and z
+            local modifier = self.object:get_properties().collisionbox[4]*3
+            
+
+            local pos2 = vector.add(vector.multiply(self.direction,modifier),pos)
+
+            local ray = minetest.raycast(pos, pos2, false, false)
+            
+            local pointed_thing = nil
+
+            if ray then
+                pointed_thing = ray:next()
+            end
+                
+            if pointed_thing then
+                if minetest.get_nodedef(minetest.get_node(pointed_thing.under).name, "walkable") then
+                    --print("jump")
+                    local vel = self.object:get_velocity()
+                    --self.jump_timer = 1+math.random()
+                    self.object:set_velocity(vector.new(vel.x,5,vel.z))
+                else
+                    --print("velocity check")
+                    vel = self.object:get_velocity()
+                    if (vel.x == 0 and self.direction.x ~= 0) or (vel.z == 0 and self.direction.z ~= 0) then
+                        self.object:set_velocity(vector.new(vel.x,5,vel.z))
+                    end
+                end
+            else
+                --print("velcheck 2")
+                local vel = self.object:get_velocity()
+                if (vel.x == 0 and self.direction.x ~= 0) or (vel.z == 0 and self.direction.z ~= 0) then
+                    self.object:set_velocity(vector.new(vel.x,5,vel.z))
+                end
+            end
+        end
+    end
+end
 
 function mob:on_step(dtime,moveresult)
     if self.dead then
@@ -147,9 +254,10 @@ function mob:on_step(dtime,moveresult)
         return
     end
 
-    --[[
+    
     self:move(dtime,moveresult)
     
+    --[[
     self:collision_detection()
     if self.fall_damage then
         self:fall_damage()
