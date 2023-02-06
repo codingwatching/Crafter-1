478908444c9ce2d6638e3a8e362f07967f3c53b6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-02-05 22:08:10 -0500

    Push all this broken mess for a distro hop

--- mods/test/init.lua
+++ mods/test/init.lua
@@ -28,3 +28,23 @@ minetest.register_entity("test:debug_cube", {initial_properties = {
     mesh = "debug_cube.gltf",
     textures = {"debug_cube.png"}
 }})
+
+
+minetest.register_entity("test:c", {
+    initial_properties = {
+        visual = "mesh",
+        mesh = "debug_character.gltf",
+        textures = {"debug_character.png"}
+    },
+    triggered = false,
+    timer = 0,
+    on_step = function(self,dtime)
+        if self.triggered then return end
+        self.timer = self.timer + dtime
+        if self.timer > 2 then
+            print("triggered")
+            self.triggered = true
+            self.object:set_animation({x=168,y=188}, 10, 0, true)
+        end
+    end
+})
