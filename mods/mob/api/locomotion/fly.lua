83e0fad8c837ad1c1b2c78bc98f7e93fab47f762
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-01 23:15:47 -0400

    Put down boilerplate

---
+++ mods/mob/api/locomotion/fly.lua
@@ -0,0 +1,9 @@
+--- Builds flying locomotion methods & fields into the mob.
+---@param mob mob
+---@param definition table
+---@return mob mob
+return function(mob, definition)
+
+
+    return mob;
+end
